d4d65d58a71e882d631926e11b423b74ca60928e
mcjwsn <maciek.wisniewski04@gmail.com>
2024-12-01 20:13:33 +0100

    cz4

--- oolab/src/main/java/agh/ics/oop/Simulation.java
+++ oolab/src/main/java/agh/ics/oop/Simulation.java
@@ -27,7 +27,7 @@ public class Simulation implements Runnable {
         this.map = map;
     }
     public void run(){
-        System.out.println(map);
+        //System.out.println(map);
             for(int i = 0; i < directions.size(); i++){
                 map.move(animals.get(i % animals.size()), directions.get(i));
                 //System.out.println(map);
--- oolab/src/main/java/agh/ics/oop/model/SimulationEngine.java
+++ oolab/src/main/java/agh/ics/oop/model/SimulationEngine.java
@@ -4,9 +4,16 @@ import agh.ics.oop.Simulation;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+
 public class SimulationEngine {
     private final List<Simulation> simulations;
     private final List<Thread> threads = new ArrayList<>();
+    private final ExecutorService threadPool = Executors.newFixedThreadPool(4);
+
     public SimulationEngine(List< Simulation> simulations) {
         this.simulations = simulations;
     }
@@ -29,9 +36,20 @@ public class SimulationEngine {
             for (Thread thread: threads){
                 thread.join();
             }
+            threadPool.shutdown();
+            if (!threadPool.awaitTermination(10, TimeUnit.SECONDS)) {
+                threadPool.shutdownNow();
+            }
         }
         catch (InterruptedException e){
-            System.out.println(e);
+            System.out.println(e.getMessage());
         }
+    }
+
+    public void runAsyncInThreadPool() {
+        for (Simulation simulation : simulations) {
+            threadPool.submit(simulation);
+        }
+        awaitSimulationEnd();
     }
 }
